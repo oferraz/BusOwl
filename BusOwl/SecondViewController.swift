a94927d5ec250839e233862c5b415876153b4b6b
Ofer Raz <ofer@coverwyze.com>
2020-03-22 19:33:52 -0700

    Initial Commit

---
+++ BusOwl/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  BusOwl
+//
+//  Created by Ofer Raz on 3/22/20.
+//  Copyright © 2020 Ofer Raz. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BusOwlTests/BusOwlTests.swift
@@ -0,0 +1,34 @@
+//
+//  BusOwlTests.swift
+//  BusOwlTests
+//
+//  Created by Ofer Raz on 3/22/20.
+//  Copyright © 2020 Ofer Raz. All rights reserved.
+//
+
+import XCTest
+@testable import BusOwl
+
+class BusOwlTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
